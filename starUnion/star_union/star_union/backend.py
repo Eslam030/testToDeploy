2248f9e11f5d9afc36548df0a0693b69459b6da2
Eslam Sayed <esla889900@gmail.com>
2024-03-04 01:10:26 +0200

    test 40

--- starUnion/star_union/star_union/backend.py
+++
@@ -1,9 +0,0 @@
-from typing import Any
-from django.contrib.auth.backends import BaseBackend
-from django.contrib.auth.base_user import AbstractBaseUser
-from django.http import HttpRequest
-
-
-class starBackEnd (BaseBackend):
-    def authenticate(self, request: HttpRequest, username: str | None = ..., password: str | None = ..., **kwargs: Any) -> AbstractBaseUser | None:
-        return super().authenticate(request, username, password, **kwargs)
